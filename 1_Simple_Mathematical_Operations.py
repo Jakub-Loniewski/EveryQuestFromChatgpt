650ef8f128df0217b25b6633045842b16f89018f
Jakub Loniewski <87779513+Jakub-Loniewski@users.noreply.github.com>
2024-10-21 21:40:09 +0200

    Add files via upload

---
+++ 1_Simple_Mathematical_Operations.py
@@ -0,0 +1,33 @@
+#Quest
+#Write a Python program that prompts the user for two numbers
+#And then displays the result of basic mathematical operations on those numbers
+#addition, subtraction, multiplication, and division.
+
+print("Hello in 4 simply mathematic operations")
+
+Number1 = float(input("enter the first number: "))
+Number2 = float(input("enter the second number: "))
+
+#I did the addition
+Addition = (Number1 + Number2)
+print(f"I did the addition and the result is: {Addition}")
+
+#I did the subtraction
+
+Subtraction = (Number1 - Number2)
+print(f"I did subtraction the result is: {Subtraction}")
+
+#I did the multiplication
+
+Multiplication = (Number1*Number2)
+print(f"I did Multiply the result is: {Multiplication}")
+
+#I did the division
+
+Division = (Number1 / Number2)
+print(f"I did dividing the result is: {Division}")
+
+print("End of Program")
+
+
+
