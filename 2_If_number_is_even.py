99e757d88223e3b99a90b3f27f26db6c77747fe7
Jakub Loniewski <87779513+Jakub-Loniewski@users.noreply.github.com>
2024-10-22 21:00:32 +0200

    DayQuest2

---
+++ 2_If_number_is_even.py
@@ -0,0 +1,7 @@
+number = int(input("Add some random number:"))
+print("Program check the number and say is even or not")
+
+if number %2 == 0:
+    print(f"Number {number} is even")
+else:
+    print(f"Number {number} is not even")
